bc419b5d4369d03986d460252f40390843acecf4
Ryeh20 <159731595+Ryeh20@users.noreply.github.com>
2024-02-11 18:37:43 -0500

    AllChanges

--- ToDoQueue_2/src/routes/router.jsx
+++ ToDoQueue_2/src/routes/router.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
                         element: <ToDoList/>
                     },
                     {
-                        path: "/RMCharacter",
+                        path: "/",
                         element: <RMCharacter/>
                     },
                     {
