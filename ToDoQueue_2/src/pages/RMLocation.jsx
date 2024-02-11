858e11d1f5095153deaa293297e9ccf77f1387ba
Ryeh20 <159731595+Ryeh20@users.noreply.github.com>
2024-02-11 17:55:50 -0500

    All push project 2

---
+++ ToDoQueue_2/src/pages/RMLocation.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import React from 'react'
+import '../styles/RMLocation.sass'
+
+const LocationCard = ({ location }) => (
+  <div className="cardData">
+    <h2>{location.name}</h2>
+    <div> Type:  {location.type}</div>
+    <div> Dimension: {location.dimension}</div>
+  </div>
+);
+
+const RMLocation = () => {
+
+  const [data, setData] = useState(null)
+  const [page, setPage] = useState(1)
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/location?page=' + page)
+   .then((response) => response.json())
+   .then(data => setData(data))
+  }, [page]);
+
+  const nextPage = () => {
+    setPage((page) => page + 1);
+  };
+
+  const previousPage = () => {
+    setPage((page) => page - 1);
+  };
+
+  const returnToPageOne = () => {
+    setPage((page) => page = 1);
+  }
+
+  return (
+    <div>
+      <button className="btn-API" onClick={returnToPageOne}>
+        Return to page one
+      </button>
+      <button className="btn-API" onClick={previousPage} disabled={page === 1}>
+        Previous Page
+      </button>
+      <button className="btn-API" onClick={nextPage}>
+        Next Page
+      </button>
+      <h1 className="headerLocation"> LOCATIONS </h1>
+      <div className="Entity">
+        <ul className="locationCard">
+          {data?.results?.map((location) => (
+            <LocationCard key={location.id} location={location} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default RMLocation
